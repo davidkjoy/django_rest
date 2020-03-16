3a21ff3d9f547f5456e10f3fb08ab968040a4dea
davidkjoy <kjdavidjoy09b@gmail.com>
2020-03-16 18:38:25 +0530

    'sample_project'

---
+++ project/myproject/api_basic/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+
+urlpatterns = [
+    #path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+
+]
